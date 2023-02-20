57ec1417d3834df57ec41bbae3ce8fa412405bbe
farpires <arpirez.fabio@gmail.com>
2023-02-20 15:15:19 -0300

    Add: project start

---
+++ clean.js
@@ -0,0 +1,13 @@
+// Mal Bad
+
+const n = 53;
+const tx = 0.15;
+const cat = 'T-Shirts';
+const ddmmyyy = new Date('August 15, 1965 00:00:00');
+
+//bien fine
+const numberOfUnits = 53;
+const tax = 0.15;
+const category = 'T-Shirts';
+const birthDate = new Date('August 15, 1965 00:00:00');
+
