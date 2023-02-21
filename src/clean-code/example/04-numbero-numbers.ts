dfeb39a559d3b9ca6984aba5942eca71f55ee401
farpires <arpirez.fabio@gmail.com>
2023-02-21 01:37:56 -0300

    Add: names for numbers

---
+++ src/clean-code/example/04-numbero-numbers.ts
@@ -0,0 +1,14 @@
+/**
+ * In general they can also use: min, max or total
+ */
+
+// bad
+const fruits = 3;
+const cars = 10;
+
+// better
+
+const maxFruits = 5;
+const minFruits = 1;
+const totalFruits = 3;
+const totalOfCars = 5; 
