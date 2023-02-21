c2139eed75fd15102d19e128b69ca1b4ca19d485
farpires <arpirez.fabio@gmail.com>
2023-02-21 01:57:43 -0300

    Add: names for Class

---
+++ src/clean-code/example/06-clases-class.ts
@@ -0,0 +1,17 @@
+/**
+ * las clases deben tener nombres formados por un sustantivos 
+ * o frases de sustantivos 
+ * EVITAR : nombres genéricos 
+ * *** EL NOMBRE DE LA CLASE ES LO MÁS IMPORTANTE DE LA CLASE
+ * *** FORMADO por un Sustantivo o frases de sustantivos
+ * *** No deben se ser muy genéricos 
+ * *** Usar  UpperCamelCase
+ * 
+ * ** USAR ESTAS TRES PREGUNTAS
+ * 
+ * 1- ¿ qué exactamente hace la clase ?
+ * 2- ¿ cómo exactamente esta la clase realizada cierta tarea ?
+ * 3- ¿ Hay algo específico sobre su ubicación ?
+ * 
+ * ** si algo no tiene sentido , remuevelo o refactoriza
+ * */
