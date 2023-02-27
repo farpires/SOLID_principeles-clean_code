98a7199fb94d97220794482db906d4bfcdfdd3fa
farpires <arpirez.fabio@gmail.com>
2023-02-26 21:40:03 -0300

    Add: uniformity in the project

---
+++ src/clean-code/10-project-uniformity.ts
@@ -0,0 +1,36 @@
+/**
+ * uniformidad de nuestro proyecto. problemas similares, soluciones similares.  
+ *
+ */
+
+const createProduct = () => { }
+
+const updateProduct = () => { }
+
+const deleteProduct = () => { }
+
+/** si después tenemos una nueva identidad  o una nueva tabla  */
+
+/** bad  - no debemos tener estos nombre de esta funciones es decir 
+ *  createNewUser, modifyUser, removeUser , no tiene sentido porque 
+ * no está uniforme 
+*/
+const createNewUser = () => { }
+const modifyUser = () => { }
+const removeUser = () => { }
+
+/**
+ * better
+ * 
+ */
+
+const createUser = () => { }
+
+const updateUser = () => { }
+
+const deleteUser = () => { }
+/**
+ *  -   también se deberá aplicar en la estructura de nuestro directorio
+ *  -   también se deberá aplicar a la indentación, tiene que estar bien clara
+ *      y seguir los estándares de los lenguajes de desarrollo
+ */
