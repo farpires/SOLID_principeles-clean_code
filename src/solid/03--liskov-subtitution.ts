e78c1e281299ee5ceed851e216c4465c8e350d03
farpires <arpirez.fabio@gmail.com>
2023-03-09 17:21:51 -0300

    Add( txt ): liskov-subtitution

---
+++ src/solid/03--liskov-subtitution.ts
@@ -0,0 +1,7 @@
+/**
+ *  " las funciones que utilizan punteros o referencias a clases base deben ser 
+ *    capaces de usar objetos de clase derivadas sin saberlo " -Robert C. Martin
+ * 
+ *    - "siendo "U" un subtipo de "T", cualquier instancia de T debería poder ser sustituida por cualquier instancia de "U" 
+ *      sin alterar las propiedades del sistema".
+ */ 
