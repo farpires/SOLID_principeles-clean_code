a1f22f69319eda485c65821d91e92d6247ca056f
farpires <arpirez.fabio@gmail.com>
2023-03-09 17:17:45 -0300

    Add( txt ): open and close

---
+++ src/solid/02-open-and-close.ts
@@ -0,0 +1,20 @@
+/**
+ * Open and Close: principio de abierto y cerrado 
+ * 
+ *  -   depende mucho del contexto de cómo estamos corriendo la aplicación 
+ *  -   podemos estar con diferentes frameworks , diferentes ambientes 
+ * 
+ *      "Establece que las entidades de software (clases, módulos, métodos, etc ) 
+ *       deben estar abiertas para extensión, pero cerradas para la modificación"
+ * 
+ *      La forma más sencilla de demostrar este principio es considerar un método que hace una cosa.
+ *      
+ *      el principio abierto-cerrado también se puede lograr de muchas otras maneras, incluso mediante el uso de la herencia
+ *      o mediante patrones de diseño de composición como el patrón de estrategia. 
+ * 
+ * COMO DETECTAR VIOLACION DE OPC
+ * 
+ *      -   Cambios normales afectan nuestra clase o módulo.
+ *      -   cuando una clase o módulo afecta muchas capas. ( presentación, almacenamiento. etc).
+ * 
+ */
